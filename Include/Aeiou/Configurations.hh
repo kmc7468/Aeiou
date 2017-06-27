28b0fd87d0b37dd7c1a3ae834e24223d76687246
kmc7468 <kmc7468@naver.com>
2017-06-27 17:02:06 +0900

    Update Configuration.hh -> Configurations.hh

---
+++ Include/Aeiou/Configurations.hh
@@ -0,0 +1,9 @@
+/*
+Aeiou is protected by Aeiou Public License Version 1.
+Made by kmc7468
+*/
+
+#ifndef AEIOU_HEADER_AEIOU_CONFIGURATIONS_HH
+#define AEIOU_HEADER_AEIOU_CONFIGURATIONS_HH
+
+#endif
