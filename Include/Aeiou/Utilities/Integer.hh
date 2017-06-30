a4110607264d97ef2e829f0a63848d1e94e50e47
kmc7468 <kmc7468@naver.com>
2017-06-30 20:23:40 +0900

    Update Utilities/Integer.hh

--- Include/Aeiou/Utilities/Integer.hh
+++ Include/Aeiou/Utilities/Integer.hh
@@ -5,10 +5,13 @@ Made by kmc7468
 
 #ifndef AEIOU_HEADER_UTILITIES_INTEGER_HH
 #define AEIOU_HEADER_UTILITIES_INTEGER_HH
+#include <Aeiou/Configurations.hh>
 
 #include <Aeiou/Preprocessor/Features-Supported.hh>
 #include <Aeiou/TypeTraits/MakeSigned.hh>
 #include <Aeiou/TypeTraits/MakeUnsigned.hh>
+#include <Aeiou/Utilities/NonComparable.hh>
+#include <Aeiou/Utilities/NonCopyable.hh>
 
 #include <climits>
 #include <cstddef>
@@ -74,8 +77,35 @@ namespace Aeiou
 		typedef UInt32_t UIntMax_t;
 #endif
 
-		typedef TypeTraits::MakeSigned<std::size_t>::Type IntPtr_t;
-		typedef TypeTraits::MakeUnsigned<std::size_t>::Type UIntPtr_t;
+		namespace Details
+		{
+			template<bool Condition_, typename True_, typename False_>
+			class If_ AEIOU_FINAL
+				: NonComparable, NonCopyable
+			{
+				AEIOU_NON_INHERITABLE(If_)
+
+			public:
+				typedef True_ Type;
+			};
+			template<typename True_, typename False_>
+			class If_<false, True_, False_> AEIOU_FINAL
+				: NonComparable, NonCopyable
+			{
+				AEIOU_NON_INHERITABLE(If_)
+
+			public:
+				typedef False_ Type;
+			};
+
+			typedef typename If_<sizeof(void*) == 2, Int16_t,
+				typename If_<sizeof(void*) == 4, Int32_t,
+				typename If_<sizeof(void*) == 8, Int64_t, void>
+				::Type>::Type>::Type IntPtr_Original_t_;
+		}
+
+		typedef TypeTraits::MakeSigned<Details::IntPtr_Original_t_>::Type IntPtr_t;
+		typedef TypeTraits::MakeUnsigned<Details::IntPtr_Original_t_>::Type UIntPtr_t;
 		typedef IntPtr_t IntFast_t;
 		typedef UIntPtr_t UIntFast_t;
 	}
