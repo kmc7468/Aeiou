1d55565b5bcc32c4dc53c62eaa2f784343d75f29
kmc7468 <kmc7468@naver.com>
2017-06-30 20:49:38 +0900

    Add Preprocessor/Bool.hh

--- Include/Aeiou/Preprocessor.hh
+++ Include/Aeiou/Preprocessor.hh
@@ -6,6 +6,8 @@ Made by kmc7468
 #ifndef AEIOU_HEADER_PREPROCESSOR_HH
 #define AEIOU_HEADER_PREPROCESSOR_HH
 
+#include <Aeiou/Preprocessor/Bool.hh>
 #include <Aeiou/Preprocessor/Features-Supported.hh>
+#include <Aeiou/Preprocessor/Identity.hh>
 
 #endif
---
+++ Include/Aeiou/Preprocessor/Identity.hh
@@ -0,0 +1,11 @@
+/*
+Aeiou is protected by Aeiou Public License Version 1.
+Made by kmc7468
+*/
+
+#ifndef AEIOU_HEADER_PREPROCESSOR_IDENTITY_HH
+#define AEIOU_HEADER_PREPROCESSOR_IDENTITY_HH
+
+#define AEIOU_PP_IDENTITY(value) value
+
+#endif
