9c3d9a2e5fd7cd2f51658fce8831016eec725fe9
kmc7468 <kmc7468@naver.com>
2017-07-01 15:11:56 +0900

    Add AeiouScript

---
+++ Include/AeiouScript/AeiouScript.hh
@@ -0,0 +1,9 @@
+/*
+AeiouScript is protected by Aeiou Public License Version 1.
+Made by kmc7468
+*/
+
+#ifndef AEIOUSCRIPT_HEADER_AEIOUSCRIPT_HH
+#define AEIOUSCRIPT_HEADER_AEIOUSCRIPT_HH
+
+#endif
