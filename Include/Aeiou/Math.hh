5070a34b1fe8963d92f2a42e65762866ba4a49ec
kmc7468 <kmc7468@naver.com>
2017-07-08 10:59:36 +0900

    Add Math/Constants.hh

--- Include/Aeiou/Math.hh
+++ Include/Aeiou/Math.hh
@@ -6,4 +6,6 @@ Made by kmc7468
 #ifndef AEIOU_HEADER_MATH_HH
 #define AEIOU_HEADER_MATH_HH
 
+#include <Aeiou/Math/Constants.hh>
+
 #endif
---
+++ Include/Aeiou/Math/Constants.hh
@@ -0,0 +1,24 @@
+/*
+Aeiou is protected by Aeiou Public License Version 1.
+Made by kmc7468
+*/
+
+#ifndef AEIOU_HEADER_MATH_CONSTANTS_HH
+#define AEIOU_HEADER_MATH_CONSTANTS_HH
+#include <Aeiou/Configurations.hh>
+
+namespace Aeiou
+{
+	namespace Math
+	{
+		AEIOU_CONSTEXPR const long double e		= 2.7182818L;
+		AEIOU_CONSTEXPR const long double PI	= 3.1415926L;
+		AEIOU_CONSTEXPR const long double g		= 9.80665L;
+	}
+
+	using Math::e;
+	using Math::PI;
+	using Math::g;
+}
+
+#endif
